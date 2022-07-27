24f2d7f5b0c901f6139d19f1bafed882f63f0134
Mario Camelo <mariuo@gmail.com>
2022-07-27 17:23:16 -0400

    Responsive style

--- front-web/src/index.tsx
+++ front-web/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 //import ReactDOM from 'react-dom/client';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './responsive.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
